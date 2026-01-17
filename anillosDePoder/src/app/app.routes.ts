9c02319afadb051222a02f3d2ac06535790f7043
Diego <diegogarciaangel25@gmail.com>
2026-01-17 18:47:36 +0100

    buscar raza por longevidad

--- anillosDePoder/src/app/app.routes.ts
+++ anillosDePoder/src/app/app.routes.ts
@@ -4,11 +4,14 @@ import { Busqueda } from './anillo/busqueda/busqueda';
 import { DetalleRaza } from './raza/detalle-raza/detalle-raza';
 import { BusquedaRazaNombre } from './raza/buscar-raza-nombre/buscar-raza-nombre';
 import { BusquedaRazaRegion } from './raza/buscar-raza-region/buscar-raza-region';
+import { BusquedaRazaLongevidad } from './raza/buscar-raza-longevidad/buscar-raza-longevidad';
 
 export const routes: Routes = [
     { path: 'detalle', component: Detalle },
     { path: 'buscar', component: Busqueda },
     { path: 'detalleRaza', component: DetalleRaza},
     { path: 'buscarRazaPorNombre', component: BusquedaRazaNombre},
-    { path: 'buscarRazaPorRegion', component: BusquedaRazaRegion}
+    { path: 'buscarRazaPorRegion', component: BusquedaRazaRegion},
+    { path: 'buscarRazaPorLongevidad', component: BusquedaRazaLongevidad},
+
 ];
