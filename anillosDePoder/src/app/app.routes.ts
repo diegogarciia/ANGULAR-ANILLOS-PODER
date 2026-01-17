1b029e28975aed37fe783eea2bd413a3cef7c2fa
Diego <diegogarciaangel25@gmail.com>
2026-01-17 18:44:16 +0100

    buscar raza por región

--- anillosDePoder/src/app/app.routes.ts
+++ anillosDePoder/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Detalle } from './anillo/detalle/detalle';
 import { Busqueda } from './anillo/busqueda/busqueda';
 import { DetalleRaza } from './raza/detalle-raza/detalle-raza';
 import { BusquedaRazaNombre } from './raza/buscar-raza-nombre/buscar-raza-nombre';
+import { BusquedaRazaRegion } from './raza/buscar-raza-region/buscar-raza-region';
 
 export const routes: Routes = [
     { path: 'detalle', component: Detalle },
     { path: 'buscar', component: Busqueda },
     { path: 'detalleRaza', component: DetalleRaza},
     { path: 'buscarRazaPorNombre', component: BusquedaRazaNombre},
+    { path: 'buscarRazaPorRegion', component: BusquedaRazaRegion}
 ];
---
+++ anillosDePoder/src/app/raza/buscar-raza-region/buscar-raza-region.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { Razas } from '../../clases/razas';
+import { Raza } from '../../interfaces/raza';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+
+@Component({
+  selector: 'app-busqueda',
+  imports: [InputTextModule,FormsModule,ButtonModule,CommonModule,RouterLink],
+  templateUrl: './buscar-raza-region.html',
+  styleUrl: './buscar-raza-region.css',
+})
+export class BusquedaRazaRegion {
+
+  raza = new Razas()
+
+
+  razasFiltradas: Raza[] = this.raza.razas
+  campoBusqueda: string = '';
+  buscarRazaRegion() {
+
+    const t = this.campoBusqueda.toLowerCase();
+
+    this.razasFiltradas = this.raza.razas.filter(r =>
+      r.regionPrincipal.toLowerCase().includes(t)
+    );
+
+  }
+}
