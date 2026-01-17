36132287858fc9947bd4cfa928b60a63cf1fab6d
Diego <diegogarciaangel25@gmail.com>
2026-01-17 18:08:37 +0100

    buscar raza por nombre

--- anillosDePoder/src/app/app.routes.ts
+++ anillosDePoder/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Detalle } from './anillo/detalle/detalle';
 import { Busqueda } from './anillo/busqueda/busqueda';
 import { DetalleRaza } from './raza/detalle-raza/detalle-raza';
+import { BusquedaRazaNombre } from './raza/buscar-raza-nombre/buscar-raza-nombre';
 
 export const routes: Routes = [
     { path: 'detalle', component: Detalle },
     { path: 'buscar', component: Busqueda },
-    { path: 'detalleRaza', component: DetalleRaza}
+    { path: 'detalleRaza', component: DetalleRaza},
+    { path: 'buscarRazaPorNombre', component: BusquedaRazaNombre},
 ];
--- anillosDePoder/src/app/raza/detalle-raza/detalle-raza.ts
+++ anillosDePoder/src/app/raza/detalle-raza/detalle-raza.ts
@@ -5,9 +5,10 @@ import { TextareaModule } from 'primeng/textarea';
 import { SelectButtonModule } from 'primeng/selectbutton';
 import { ButtonModule } from 'primeng/button';
 import { SelectModule } from 'primeng/select';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-detalle-raza',
-  imports: [ReactiveFormsModule, SelectModule,InputTextModule,TextareaModule,SelectButtonModule,ButtonModule],
+  imports: [ReactiveFormsModule, SelectModule,InputTextModule,TextareaModule,SelectButtonModule,ButtonModule, RouterLink],
   templateUrl: './detalle-raza.html',
   styleUrl: './detalle-raza.css',
 })
